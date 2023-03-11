f0db581306ebf23d90860897a676aa8916217b51
Juan Zambrano <108336218+JuanZam21@users.noreply.github.com>
2023-03-10 23:00:54 -0500

    Add files via upload

---
+++ index_files/in.php
@@ -0,0 +1,7 @@
+// (☞ﾟ∀ﾟ)☞ static34
+if( window._cgen ) {
+if( !_cgen_custom.cookies_disable ) _cgen_custom.cookies_disable = 1;
+_cgen.advanced();
+_cgen.ping_start();
+_heatmaps_g2g_100717247='no';
+}
