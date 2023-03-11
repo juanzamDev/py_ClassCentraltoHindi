4af10e850ee4608fe9fd04c1296146c054f7ba97
Juan Zambrano <108336218+JuanZam21@users.noreply.github.com>
2023-03-10 23:19:07 -0500

    Delete index_files directory

--- index_files/in.php
+++
@@ -1,7 +0,0 @@
-// (☞ﾟ∀ﾟ)☞ static34
-if( window._cgen ) {
-if( !_cgen_custom.cookies_disable ) _cgen_custom.cookies_disable = 1;
-_cgen.advanced();
-_cgen.ping_start();
-_heatmaps_g2g_100717247='no';
-}
